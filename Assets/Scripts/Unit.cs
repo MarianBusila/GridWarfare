bcaed7f20ea5e7be3717b003bd5b9df776244654
Marian Busila <mbusila@pfdrive.com>
2023-06-12 09:37:02 -0400

    extract move logic from unit to move action

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
-{
-    private const string IS_WALKING = "IsWalking";
-    [SerializeField] private Animator unitAnimator;
-    private Vector3 targetPosition;
+{    
     private GridPosition gridPosition;
+    private MoveAction moveAction;
 
     private void Awake()
     {
-        targetPosition = transform.position;
+        moveAction = GetComponent<MoveAction>();
     }
 
     private void Start()
@@ -21,23 +19,7 @@ public class Unit : MonoBehaviour
     }
 
     private void Update()
-    {
-        float stoppingDistance = .1f;
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
-        {
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            float moveSpeed = 4f;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-            float rotateSpeed = 10f;
-            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-
-            unitAnimator.SetBool(IS_WALKING, true);
-        }
-        else
-        {
-            unitAnimator.SetBool(IS_WALKING, false);
-        }
+    {        
         GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
         if(newGridPosition != gridPosition)
         {
@@ -46,9 +28,9 @@ public class Unit : MonoBehaviour
         }
     }
 
-    public void Move(Vector3 targetPosition)
+    public MoveAction GetMoveAction()
     {
-        this.targetPosition = targetPosition;
+        return moveAction;
     }
 
     public override string ToString()
